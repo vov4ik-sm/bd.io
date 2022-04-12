e6c194c1e3f76a100d3cd0a7c662e867f4ce4690
vov4ik-sm <90687054+vov4ik-sm@users.noreply.github.com>
2022-04-12 18:58:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form action="Запрос_client_lada.php" method="GET">
 <h1>Управление базой данных "dealer"</h1>
-<h2>Вам доступны следующие функции:"</h2>
+<h2>Вам доступны следующие функцииb:"</h2>
 Запрос №3: Запрос_insert_brand
 <form method="post" action="Запрос_insert_brand.php">
 <input type="submit" name="submitButton" value="Перейти" />
